d2239072b7de9852a1416428cd822100b8387dd6
jitka <ji.knizkova@gmail.com>
2026-04-09 11:36:46 +0200

    vytvoření testovací databáze

--- test_projekt2.py
+++ test_projekt2.py
@@ -10,13 +10,20 @@ def db_spojeni():
     conn = mysql.connector.connect(host="127.0.0.1", user="root", password="1111", database="sys")
     cursor = conn.cursor()
 
+    cursor.execute("CREATE DATABASE IF NOT EXISTS test_projekt2_db")
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS task_manager (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nazev VARCHAR(100),
+                popis VARCHAR(200),
+                stav ENUM('nezahajeno', 'probiha', 'hotovo'),
+                datum_vytvoreni DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+                   ''')
+    conn.commit()
     yield conn, cursor 
     
-    # úklid
-    cursor.execute("DELETE FROM task_manager WHERE nazev = 'TEST_UKOL'")
-    conn.commit()
-    cursor.close()
-    conn.close()
+
 
 # pozitivní test funkce pridat_ukol
 @patch('builtins.input', side_effect=["TEST_UKOL", "Toto je testovací popis"])
@@ -38,3 +45,11 @@ def test_pridat_ukol_pozitivni(mock_input, db_spojeni):
     assert len(vysledek) == 1  
 
 
+
+
+
+    # úklid
+    cursor.execute("DROP DATABASE test_projekt2_db")
+    conn.commit()
+    cursor.close()
+    conn.close()
